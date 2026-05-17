7658e9792ed52d81ae63d6f4208d7a2ddfe4afa4
Anirudh Singh Shekhawat <rodiyarrr@gmail.com>
2026-05-17 14:41:48 +0530

    Handled same file name collision and Share Code Generation

--- src/main/java/com/project/peerdrop/controller/FileController.java
+++ src/main/java/com/project/peerdrop/controller/FileController.java
@@ -1,4 +1,27 @@
 package com.project.peerdrop.controller;
 
+import com.project.peerdrop.dto.request.UploadRequestDTO;
+import com.project.peerdrop.dto.response.AuthResponseDTO;
+import com.project.peerdrop.dto.response.FileResponseDTO;
+import com.project.peerdrop.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/file")
 public class FileController {
+
+    @Autowired
+    private FileService fileService;
+
+    @PostMapping("/upload")
+    public FileResponseDTO uploadFile(@ModelAttribute UploadRequestDTO requestDTO) throws IOException {
+
+        return fileService.uploadFile(requestDTO);
+    }
 }
--- src/main/java/com/project/peerdrop/service/FileService.java
+++ src/main/java/com/project/peerdrop/service/FileService.java
@@ -25,32 +25,54 @@ public class FileService {
 
         SharedFile sharedFile=new SharedFile();
 
+        // Taking file as input
         MultipartFile file= requestDTO.getFile();
         Long fileSize= file.getSize();
 
+        //File transfer logic - /uploads
+        String uploadPath = System.getProperty("user.dir") + "/uploads/";
+        File uploadDirectory=new File(uploadPath);
+
+        if (!uploadDirectory.exists()){
+            uploadDirectory.mkdirs();
+        }
+        File destinationFile=new File(uploadDirectory,
+                sharedFile.getStoredFileName());
+
+        file.transferTo(destinationFile);
+
+
+        //Share Code generation
+        String shareCode =String.valueOf((int) (Math.random()*900000)+100000);
+        sharedFile.setShareCode(shareCode);
+
+        //File-Metadata
         String originalFileName= file.getOriginalFilename();
         sharedFile.setOriginalFileName(originalFileName);
+        sharedFile.setMimeType(file.getContentType());
+        sharedFile.setFileSize(fileSize);
+        sharedFile.setUploadedAt(LocalDateTime.now());
 
+        //Unique File Name
         String storedFileName=System.currentTimeMillis()+"_"+originalFileName;
         sharedFile.setStoredFileName(storedFileName);
 
-        sharedFile.setFileSize(fileSize);
+        //CORE SHARING LOGIC
         sharedFile.setMaxDownloadLimit(requestDTO.getNoOfDownloadsAllowed());
-        //ye aage jake BCrypt use krke hash krna hai pw before storing
         sharedFile.setFilePasswordHash(requestDTO.getPassword());
         sharedFile.setExpiryTime(requestDTO.getExpiryTime());
-        sharedFile.setUploadedAt(LocalDateTime.now());
         sharedFile.setDownloadCount(0);
 
-        String uploadPath="uploads/";
-        File destinationFile=new File(uploadPath,sharedFile.getStoredFileName());
-        file.transferTo(destinationFile);
 
+        //SAVE TO DB
         sharedFileRepository.save(sharedFile);
 
+        //Response sent to client after File Upload
         FileResponseDTO responseDTO=new FileResponseDTO();
         responseDTO.setExpiryTime(sharedFile.getExpiryTime());
         responseDTO.setFileName(sharedFile.getOriginalFileName());
+        responseDTO.setShareCode(shareCode);
+
         return responseDTO;
 
     }
